86e4d245f02d86475cff22fd2f1e7c0168b596c4
Nguyen Tan Dat <ntd2003999@gmail.com>
2024-03-25 01:26:37 +0700

    Changing port in server, coding conponents Header Navigation and Sidebar

---
+++ client/src/utils/icons.js
@@ -0,0 +1,15 @@
+import { RiPhoneFill } from "react-icons/ri";
+import { MdEmail } from "react-icons/md";
+import { BsFillHandbagFill } from "react-icons/bs";
+import { BiSolidUserCircle } from "react-icons/bi";
+import { AiFillHome } from "react-icons/ai";
+
+const icons = {
+    RiPhoneFill,
+    MdEmail,
+    BsFillHandbagFill,
+    BiSolidUserCircle,
+    AiFillHome,
+};
+
+export default icons;
