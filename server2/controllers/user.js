1f4b0a9aebf718181e55619bc61c6cb4fb09fddf
unknown <22540006@gm.uit.edu.vn>
2024-03-26 00:10:27 +0700

    #3update

--- server2/controllers/user.js
+++ server2/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user')
 const asyncHandler = require('express-async-handler')
+const { generateAccessToken, generateRefreshToken } = require('../middlewares/jwt')
 const register = asyncHandler(async(req, res) => {
     const {email, password, firstname, lastname} = req.body
     if (!email || !password || !lastname || !firstname)
@@ -7,13 +8,61 @@ const register = asyncHandler(async(req, res) => {
         success: false,
         mes: 'Missing inputs'
 })
-    const response = await User.create(req.body)
-    return res.status(200).json({
-        sucess: response ? true : false,
-        response
-    })
+
+    const user = await User.findOne({email})
+    if (user)
+        throw new Error('User has existed!')
+    else{
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            success: newUser ? true : false,
+            mes: newUser ? 'Register is sucessfully. Please go login~' : 'Something went wrong'
+        })
+    }
+
+})
+// Refresh token => Cap moi access token
+// Access token => Xac thuc nguoi dung, phan quyen nguoi dung
+const login = asyncHandler(async(req, res) => {
+    const {email, password} = req.body
+    if (!email || !password)
+    return res.status(400).json({
+        success: false,
+        mes: 'Missing inputs'
+})
+    const response = await User.findOne({email})
+    //console.log(await response.isCorrectPassword(password));
+    if (response && await response.isCorrectPassword(password)){
+        //Tach password va role ra khoi response
+        const {password, role, ...userData} = response.toObject()
+        //Tao access tken
+        const accessToken = generateAccessToken(response._id, role)
+        // Tao refresh token
+        const refreshToken = generateRefreshToken(response._id)
+        //Luu refresh token vao database
+        await User.findByIdAndUpdate(response._id, {refreshToken}, {new:true})
+        //Luu refresh token vao cookie
+        res.cookie('refreshToken', refreshToken, {httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000})
+        return res.status(200).json({
+            success: true,
+            accessToken,
+            userData
+        })
+    }else{
+        throw new Error('Invalid credentials')
+    }
 })
 
+const getCurrent = asyncHandler(async(req, res) => {
+    const {_id} = req.user
+    const user = await User.findById(_id)
+    return res.status(200).json({
+        success: false,
+        rs: user ? user : 'User not found'
+    })
+})
 module.exports = {
-    register
+    register,
+    login,
+    getCurrent
 }
---
+++ server2/middlewares/verifyToken.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+
+// Bearer token
+// headers: { authorization: Bearer token}
+const verifyAccessToken = asyncHandler(async(req, res, next) => {
+    if(req?.headers?.authorization?.startswitch('Bearer')){
+        const token = req.headers.authorization.split('')[1]
+        jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+            if (err) return res.status(401).json({
+                sucess: false,
+                mes: 'Invalid acess token'
+            })
+            console.log(decode);
+            req.user = decode
+            next()
+        })
+    } else {
+        return res.status(401).json({
+            sucess: false,
+            mes: 'Require authentication!!!'
+        })
+    }
+})
+
+
+module.exports = {
+    verifyAccessToken
+}
