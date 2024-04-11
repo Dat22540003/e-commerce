7a0550eb8af5f8aae27a92e7d0742f7f4c15e079
unknown <22540006@gm.uit.edu.vn>
2024-04-11 17:21:11 +0700

    #update1104

---
+++ server2/routes/blogCategory.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+const ctrls = require('../controllers/blogCategory')
+const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
+
+router.post('/', [verifyAccessToken, isAdmin], ctrls.createCategory)
+router.get('/', ctrls.getCategory)
+router.put('/:bcid', [verifyAccessToken, isAdmin], ctrls.updateCategory)
+router.delete('/:bcid', [verifyAccessToken, isAdmin], ctrls.deleteCategory)
+
+
+ 
+
+module.exports = router
+
+
+// CRUD | Create - Read - Update - Delete | POST - GET - PUT - DELETE
+// CREATE (POST) + PUT - BODY
+// GET + DELETE - QUERY // ?ddd&fdfs
